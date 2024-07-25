b380cd6ff32df16ba718be28e2001ee430ea0031
Ayoub Aabass <ayoub@aabass.net>
2024-07-25 18:14:47 +0100

    feat: added Vercel toolbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,54 +1,54 @@
-import nextMDX from '@next/mdx'
-import remarkFrontmatter from 'remark-frontmatter';
+import nextMDX from "@next/mdx";
+import withVercelToolbar from "@vercel/toolbar/plugins/next";
+import remarkFrontmatter from "remark-frontmatter";
 
 const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {   
-    providerImportSource: "@mdx-js/react",
-    rehypePlugins: [],
-    remarkPlugins: [remarkFrontmatter],
-  },
-})
-
+	extension: /\.mdx?$/,
+	options: {
+		providerImportSource: "@mdx-js/react",
+		rehypePlugins: [],
+		remarkPlugins: [remarkFrontmatter],
+	},
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'it'],
-  },
-  images: {
-    domains: [
-      "media.graphassets.com"
-    ]
-  },
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack(config) {
-    config.module.rules.push({
-      issuer: /\.[jt]sx?$/,
-      test: /\.svg$/i,
-      use: [{
-        loader: "@svgr/webpack",
-        options: {
-          svgoConfig: {
-            plugins: [
-              {
-                active: false, 
-                name: 'cleanupIDs',
-              },
-              {
-                active: false, 
-                name: 'collapseGroups',
-              }
-            ]
-          }
-        }
-      }]
-    })
-    return config
-  }
-}
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "it"],
+	},
+	images: {
+		domains: ["media.graphassets.com"],
+	},
+	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+	reactStrictMode: true,
+	swcMinify: true,
+	webpack(config) {
+		config.module.rules.push({
+			issuer: /\.[jt]sx?$/,
+			test: /\.svg$/i,
+			use: [
+				{
+					loader: "@svgr/webpack",
+					options: {
+						svgoConfig: {
+							plugins: [
+								{
+									active: false,
+									name: "cleanupIDs",
+								},
+								{
+									active: false,
+									name: "collapseGroups",
+								},
+							],
+						},
+					},
+				},
+			],
+		});
+		return config;
+	},
+};
 
-export default withMDX(nextConfig)
+export default withVercelToolbar()(withMDX(nextConfig));
