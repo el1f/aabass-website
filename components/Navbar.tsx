b0223dc63d57ac78a577d7a9f881d43c5c8204e8
Ayoub Aabass <ayoub@aabass.net>
2024-08-12 16:42:20 +0100

    fix: layering issues

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import classNames from "classnames";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useMemo } from "react";
 import { twMerge } from "tailwind-merge";
 
 import { SOCIALS } from "../data";
@@ -54,102 +54,132 @@ const SpotifyWidget: React.FC = () => {
 		</div>
 	);
 
-	if (isLoading && fetchStatus !== "idle")
-		return (
-			<div className="flex flex-col">
+	const widgetContent = useMemo(() => {
+		if (isLoading && fetchStatus !== "idle")
+			return (
 				<div className="p-3 mb-4 rounded-md bg-bgRaised">
 					<Text size="sm">Loading</Text>
 				</div>
+			);
 
-				{musicLinks}
-			</div>
-		);
-
-	if (!currentlyPlayingSong)
-		return (
-			<div className="flex flex-col">
+		if (!currentlyPlayingSong)
+			return (
 				<div className="p-3 mb-4 rounded-md bg-bgRaised">
 					<Text size="sm">
 						Currently listening to my thoughts instead of some sick beats
 					</Text>
 				</div>
+			);
 
-				{musicLinks}
-			</div>
-		);
+		if (error || "error" in currentlyPlayingSong) return <p>Error</p>;
 
-	if (error || "error" in currentlyPlayingSong) return <p>Error</p>;
-
-	const artists = currentlyPlayingSong.item.artists.map((artist) => (
-		<span
-			className="[&:not(:last-child)]:after:content-[','] [&:not(:last-child)]:after:mr-1"
-			key={artist.id}
-		>
-			<Link
-				className="hover:underline underline-offset-2"
-				href={artist.external_urls.spotify}
-				target="_blank"
+		const artists = currentlyPlayingSong.item.artists.map((artist) => (
+			<span
+				className="[&:not(:last-child)]:after:content-[','] [&:not(:last-child)]:after:mr-1"
+				key={artist.id}
 			>
-				{artist.name}
-			</Link>
-		</span>
-	));
+				<Link
+					className="hover:underline underline-offset-2"
+					href={artist.external_urls.spotify}
+					target="_blank"
+				>
+					{artist.name}
+				</Link>
+			</span>
+		));
+
+		return (
+			<>
+				<Icon className="absolute top-4 left-4 " name="spotify" />
+				<motion.figure
+					className={cn(
+						"w-64 mb-4 overflow-hidden rounded-full bg-textDimmed aspect-square relative",
+						"before:content-[attr(data-icon)] before:w-4 before:h-4 before:absolute before:top-1/2 before:left-1/2 before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:bg-bgRaised before:rounded-full",
+						"after:content-[attr(data-icon)] after:w-20 after:h-20 after:absolute after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-1/2 after:bg-black/30 after:rounded-full",
+					)}
+					transition={{
+						ease: "easeOut",
+					}}
+					variants={{
+						hidden: { opacity: 0, scale: 1.2 },
+						visible: { opacity: 1, scale: 1 },
+					}}
+				>
+					<Image
+						alt="Album cover"
+						className={cn(
+							{ pause: !currentlyPlayingSong.is_playing },
+							"animate-spin-slow",
+						)}
+						height={256}
+						src={currentlyPlayingSong.item.album.images[0].url}
+						width={256}
+					/>
+				</motion.figure>
+				<Text className="line-clamp-1">
+					<Link
+						className="hover:underline underline-offset-2"
+						href={currentlyPlayingSong.item.external_urls.spotify}
+						target="_blank"
+					>
+						{currentlyPlayingSong.item.name}
+					</Link>
+				</Text>
+				<Text className="mb-2" size="sm">
+					<Link
+						className="hover:underline underline-offset-2"
+						href={currentlyPlayingSong.item.album.external_urls.spotify}
+						target="_blank"
+					>
+						{currentlyPlayingSong.item.album.name}
+					</Link>
+					{" • "}
+					{artists}
+				</Text>
+
+				{/* Play progress */}
+				<div className="w-full h-0.5 pointer-events-none relative bg-textDimmedDark/50 rounded overflow-hidden mb-4">
+					<motion.span
+						animate={{
+							width:
+								(currentlyPlayingSong.progress_ms /
+									currentlyPlayingSong.item.duration_ms) *
+									100 +
+								"%",
+						}}
+						className="absolute bottom-0 left-0 h-0.5 bg-primary-400"
+					/>
+				</div>
+			</>
+		);
+	}, [currentlyPlayingSong, error, fetchStatus, isLoading]);
 
 	return (
-		<div className="flex flex-col">
-			<Icon className="absolute top-4 left-4 " name="spotify" />
-			<figure
-				className={cn(
-					"w-64 mb-4 overflow-hidden rounded-full bg-textDimmed aspect-square relative animate-spin-slow",
-					"before:content-[attr(data-icon)] before:w-4 before:h-4 before:absolute before:top-1/2 before:left-1/2 before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:bg-bgRaised before:rounded-full",
-					"after:content-[attr(data-icon)] after:w-20 after:h-20 after:absolute after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-1/2 after:bg-black/30 after:rounded-full",
-					{ pause: !currentlyPlayingSong.is_playing },
-				)}
-			>
-				<Image
-					alt="Album cover"
-					height={256}
-					src={currentlyPlayingSong.item.album.images[0].url}
-					width={256}
-				/>
-			</figure>
-			<Text className="line-clamp-1">
-				<Link
-					className="hover:underline underline-offset-2"
-					href={currentlyPlayingSong.item.external_urls.spotify}
-					target="_blank"
-				>
-					{currentlyPlayingSong.item.name}
-				</Link>
-			</Text>
-			<Text className="mb-2" size="sm">
-				<Link
-					className="hover:underline underline-offset-2"
-					href={currentlyPlayingSong.item.album.external_urls.spotify}
-					target="_blank"
-				>
-					{currentlyPlayingSong.item.album.name}
-				</Link>
-				{" • "}
-				{artists}
-			</Text>
-
-			{/* Play progress */}
-			<div className="w-full h-0.5 pointer-events-none relative bg-textDimmedDark/50 rounded overflow-hidden mb-4">
-				<motion.span
-					animate={{
-						width:
-							(currentlyPlayingSong.progress_ms /
-								currentlyPlayingSong.item.duration_ms) *
-								100 +
-							"%",
-					}}
-					className="absolute bottom-0 left-0 h-0.5 bg-primary-400"
-				/>
+		<motion.div
+			animate="visible"
+			className="p-4 overflow-hidden border rounded-lg bg-bgRaisedLight/50 dark:bg-bgRaisedDark/50 backdrop-blur-md dark:border-textLight/10 border-textDark/10"
+			exit="hidden"
+			initial="hidden"
+			style={{
+				perspective: 500,
+			}}
+			variants={{
+				hidden: { opacity: 0, scale: 0.9, y: -32 },
+				visible: {
+					opacity: 1,
+					scale: 1,
+					transition: {
+						delayChildren: 0.3,
+					},
+					y: 0,
+				},
+			}}
+		>
+			<div className="flex flex-col w-64">
+				{widgetContent}
+				{musicLinks}
 			</div>
-
-			{musicLinks}
-		</div>
+		</motion.div>
 	);
 };
 
@@ -230,9 +260,11 @@ export const Navbar: React.FC<NavbarProps> = () => {
 										)}
 									</span>
 								</PopoverTrigger>
-								<PopoverContent className="overflow-hidden border bg-bgRaisedLight/50 dark:bg-bgRaisedDark/50 backdrop-blur-md dark:border-textLight/10 border-textDark/10">
-									<SpotifyWidget />
-								</PopoverContent>
+								<AnimatePresence>
+									<PopoverContent asChild>
+										<SpotifyWidget />
+									</PopoverContent>
+								</AnimatePresence>
 							</Popover>
 
 							{SOCIALS.map((social) => (
