9bbfac821643148dbcea156b1d366c6319a3d437
Ayoub Aabass <ayoub@aabass.net>
2026-08-06 16:32:13 +0100

    Point Tailwind content globs at the app directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./app/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	darkMode: "class",
